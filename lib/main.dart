ea46f8100be9fa65e06f3d4544d2c29df4c37623
Steven Spiel <sspiel@transcarent.ai>
2021-06-28 16:16:02 -0400

    minimum reproduction of code

--- lib/main.dart
+++ lib/main.dart
@@ -4,189 +4,40 @@ import 'package:get/get.dart';
 void main() {
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: '/home',
-    defaultTransition: Transition.fade,
-    getPages: [
-      GetPage(
-        name: '/home',
-        page: () => HomePage(),
-        binding: HomeBinding(),
-      ),
-      GetPage(
-        name: '/another',
-        page: () => AnotherPage(),
-      ),
-    ],
+    home: Home(),
   ));
 }
 
-class HomeBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => HomeController());
-  }
+class Page1Controller extends GetxController {
+  var index = 0.obs;
 }
 
-class HomeController extends GetxController {
-  static HomeController get to => Get.find();
-
-  var currentIndex = 0.obs;
-
-  final pages = <String>['/browse', '/history', '/settings'];
-
-  void changePage(int index) {
-    currentIndex.value = index;
-    Get.toNamed(pages[index], id: 1);
-  }
-
-  Route onGenerateRoute(RouteSettings settings) {
-    if (settings.name == '/browse')
-      return GetPageRoute(
-        settings: settings,
-        page: () => BrowsePage(),
-        binding: BrowseBinding(),
-      );
-
-    if (settings.name == '/history')
-      return GetPageRoute(
-        settings: settings,
-        page: () => HistoryPage(),
-        binding: HistoryBinding(),
-      );
-
-    if (settings.name == '/settings')
-      return GetPageRoute(
-        settings: settings,
-        page: () => SettingsPage(),
-        binding: SettingsBinding(),
-      );
-
-    return null;
-  }
-}
-
-class HomePage extends GetView<HomeController> {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Navigator(
-        key: Get.nestedKey(1),
-        initialRoute: '/browse',
-        onGenerateRoute: controller.onGenerateRoute,
-      ),
-      bottomNavigationBar: Obx(
-            () => BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Browse',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.history),
-              label: 'History',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Settings',
-            ),
-          ],
-          currentIndex: controller.currentIndex.value,
-          selectedItemColor: Colors.pink,
-          onTap: controller.changePage,
-        ),
-      ),
-    );
-  }
-}
-
-class BrowsePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Browse')),
       body: Center(
-        child: Container(
-          child: Text(Get.find<BrowseController>().title.value),
+        child: TextButton(
+          child: Text('Got to page 1'),
+          onPressed: () => Get.to(() => Page1()),
         ),
       ),
     );
   }
 }
 
-class HistoryPage extends StatelessWidget {
+class Page1 extends GetWidget<Page1Controller> {
   @override
   Widget build(BuildContext context) {
+    Get.create(() => Page1Controller());
+
     return Scaffold(
-      appBar: AppBar(title: Text('History')),
-      body: Center(
-        child: Container(
-          child: Text(Get.find<HistoryController>().title.value),
-        ),
+      appBar: AppBar(title: Text('Page 1')),
+      body: Obx(() => Center(child: Text(controller.index().toString()))),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => controller.index++,
       ),
     );
-  }
-}
-
-class SettingsPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Settings')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: Text(Get.find<SettingsController>().title.value),
-            ),
-            RaisedButton(
-              child: Text('Another Page'),
-              onPressed: () => Get.toNamed('/another'),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class BrowseController extends GetxController {
-  final title = 'Browser'.obs;
-}
-
-class HistoryController extends GetxController {
-  final title = 'History'.obs;
-}
-
-class SettingsController extends GetxController {
-  final title = 'Settings'.obs;
-}
-
-class BrowseBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => BrowseController());
-  }
-}
-
-class HistoryBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => HistoryController());
-  }
-}
-
-class SettingsBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => SettingsController());
-  }
-}
-
-class AnotherPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('Another Page')), body: Container());
   }
 }
